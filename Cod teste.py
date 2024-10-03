15e4d67779a472800746abeb98b611de1b425aaf
Noth430 <henrique2.095@outlook.com>
2024-10-03 17:41:00 +0000

    salvar

---
+++ Cod teste.py
@@ -0,0 +1,6 @@
+import os
+os.system("cls || clear")
+from dataclasses import dataclass
+
+""""
+    Foi feita uma pesquisa esntre os habitantes de uma região. Foram coletados os dados de idade,sexo(M\F) e salário.
