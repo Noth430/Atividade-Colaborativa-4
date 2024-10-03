c6f950d88bd465dfbb23c1fdf3623779845b15c8
luizacamposss <mluizafields@gmail.com>
2024-10-03 19:59:17 +0000

    salvar5

--- Cod Luiza.py
+++ Cod Luiza.py
@@ -1,132 +1,88 @@
+"""
+Maria Luiza
+Henrique Santos
+"""
+
 import os
 os.system("cls || clear")
 from dataclasses import dataclass
 
-"""
-
-Membros da equipe:
-- Maria Luiza
-- Henrique Santos
-
-"""
-""""
-    Foi feita uma pesquisa esntre os habitantes de uma região. Foram coletados os dados de idade,sexo(M\F) e salário.
-    faça um algoritmo que informe:
-
-a) a média de salário idade do grupo;
-b)maior e menor idade do grupo;
-c)quantidade de mulheres com salário a partir de R$5.000.00
-
-Crie um menu com três opções.
-
-Código  | Descrição 
-        | Adigiconar pessoa
-        | Exibir resultados
-        | Sair
-
-O final da leitura de dados se dará com quando o usuário digitar o número código 3.
-ao adicionar uma família,deve-se limpar o terminal e apresentar o menu novamente.
-
-    - Salve os dados em um arquivo com nome; pesquisa_habitantes.txt.
-    - O progama deve ler o arquivo para exibir os dados salvos.
-
- """
-
-
 @dataclass
-class Habitante:
-    idade: int
-    genero: str
-    salario: float
+class Habitantes:
+    idade:int
+    sexo:str
+    salario:float
 
 @dataclass
 class Final:
-    lista_total: list
+    lista_final:list
 
 def menu():
     print("""
-    === MENU ===
-
-1. Adicionar mais uma pessoa/família 
-2. Exibir os resultados
-3. Sair   
-          """)
-
-def sexo(dados):
-    
-    homens = []
-    mulheres = []
-    sexo = input("Escolha a opção relacionada a seu sexo: ")
-
-    match sexo:
-        case 'Homem':
-             homens.append(dados)
-        case 'Mulher':
-            mulheres.append(dados)
-    return dados
-    
-    
-
-def criando_arquivo(a,b):
-    with open(a, "a") as lista_habitantes:
-        for habitante in b:
-            lista_habitantes.write(f"{habitante.idade}, {habitante.genero}, {habitante.salario}, \n")
-        lista_habitantes.close()
-
-def criando_arquivo_final(a,b):
-    with open(a, "w") as lista_habitantes:
-        for habitante in b:
-            lista_habitantes.write(f"{habitante}, \n")
-        lista_habitantes.close()
-
-def lendo_arquivo(a):
-    with open(a, "r") as arquivos_habitantes:
-        for linha in arquivos_habitantes:
-            idade, genero, salario = linha.strip().split(",")
-            lista_total.append(Habitante(idade=int(idade), genero=str(genero),salario=float(salario)))
-        arquivos_habitantes.close()
-
-    return lista_total
-    
-def media(a):
-    QNT = len(a)
-    soma = sum(a)
-
-    if QNT == 0:
-        return 0
-    else:
-        media == soma/ QNT
-        return media
+Código  |   Descrição
+   1    |  Adicionar pessoa
+   2    |  Exibir resultados
+   3    |  Sair
+""")
 
 def limpar_tela():
-    os.system("cls || clear")
+    os.system("cls || clear ")
+
+def criando_arquivo(a, b):
+    with open(a,"a") as arquivo_habitantes:
+        for habitantes in b:
+            arquivo_habitantes.write(f"{habitantes.idade},{habitantes.sexo},{habitantes.salario}\n")
+        arquivo_habitantes.close()
+
+def criando_arquivo_final(a, b):
+    with open(a,"a") as arquivo_habitantes:
+        for habitantes in b:
+            arquivo_habitantes.write(f"{habitantes}, \n")
+        arquivo_habitantes.close()
+
+def lendo_arquivos(a):
+    with open(a, "r") as lendo_habitantes:
+        for linha in lendo_habitantes:
+            idade, sexo, salario = linha.strip().split(",")
+            lista_final.append(Habitantes(idade = int(idade), sexo = sexo, salario = float(salario)))
+        lendo_habitantes.close()
+    return lista_final
+
+def media(a):
+    QUANTIDADE = len(a)
+    soma = sum(a)
+    if QUANTIDADE == 0:
+        return 0
+    else:
+        media = soma / QUANTIDADE
+        return media
 
 while True:
-    lista_total = []
+    lista_final = []
     menu()
-    opcao = (input("Resposta: "))
+    opcao = int(input("Resposta: "))
     match opcao:
-        case "1": 
-            habitante = Habitante(
-            idade = int(input("Digite a sua idade: ")),
-            genero = input("Digite o seu gênero: "),
-            salario = float(input("Digite o valor do seu salário: ")))
-            lista_total.append(habitante)
-            nome_arquivo_habitante = "pesquisa_habitantes.txt"
-            criando_arquivo(nome_arquivo_habitante, lista_total)
+        case 1:
+            habitantes = Habitantes(
+                idade = int(input("Digite sua idade: ")),
+                sexo = input("Digite seu gênero: ").lower().strip(),
+                salario = float(input("Digite quanto de salário você recebe: ")),
+            )
+            lista_final.append(habitantes)
+            nome_arquivo = "dados.txt"
+            criando_arquivo(nome_arquivo, lista_final)
             limpar_tela()
-        case "2":
-            lista_final = []
+        case 2:
             lista_final1 = []
             contador = 0
             limpar_tela()
             lista_salario = []
             lista_idade = []
-            nome_arquivo_habitante = "pesquisa_habitantes.txt"
-            lista_final1 = lendo_arquivo(nome_arquivo_habitante)
+            nome_arquivo = "dados.txt"
+            lista_final1 = lendo_arquivos(nome_arquivo)
             for habitantes in lista_final1:
                 lista_idade.append(habitantes.idade)
-                if habitante.genero == "f" and habitante.salario >= 5000:
+                if habitantes.sexo == "f" and habitantes.salario >= 5000:
                     contador += 1
                 lista_salario.append(habitantes.salario)
                 
@@ -137,19 +93,17 @@ while True:
             lista_final.append(media_salario) 
             lista_final.append(maior_idade)
             lista_final.append(menor_idade) 
-            nome_arquivo1 = "pesquisa_habitantes_final.txt"
-            criando_arquivo_final(nome_arquivo1, lista_final1)
+            nome_arquivo = "pesquisa_habitantes.txt"
+            criando_arquivo_final(nome_arquivo, lista_final)
             print("\n=== Exibindo resultado ===")
             print(f"Média salário do grupo: {media_salario}")
             print(f"Maior idade do grupo: {maior_idade}")
             print(f"Menor idade do grupo: {menor_idade}")
             print(f"Quantidade de mulheres com salário a partir de R$ 5000,00: {contador}")
         case 3:
-            print("Ok, saindo do programa.")
+            print("Programa encerrado")
             break
         case _:
-            print("Não existe essa opção. Por favor, tente novamente.")
+            print("Opção inválida. Tente novamente")
             continue
-            
-
 
