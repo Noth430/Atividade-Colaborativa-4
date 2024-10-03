2bebf32f39d88aa2d4897fe78ff93e878278422f
Noth430 <henrique2.095@outlook.com>
2024-10-03 17:49:39 +0000

    SALVAR

--- Cod teste.py
+++ Cod teste.py
@@ -2,5 +2,32 @@ import os
 os.system("cls || clear")
 from dataclasses import dataclass
 
+"""
+
+Membros da equipe:
+- Maria Luiza
+- Henrique Santos
+
+"""
 """"
     Foi feita uma pesquisa esntre os habitantes de uma região. Foram coletados os dados de idade,sexo(M\F) e salário.
+    faça um algoritmo que informe:
+
+a) a média de salário idade do grupo;
+b)maior e menor idade do grupo;
+c)quantidade de mulheres com salário a partir de R$5.000.00
+
+Crie um menu com três opções.
+
+Código  | Descrição 
+        | Adigiconar pessoa
+        | Exibir resultados
+        | Sair
+
+O final da leitura de dados se dará com quando o usuário digitar o número código 3.
+ao adicionar uma família,deve-se limpar o terminal e apresentar o menu novamente.
+
+    - Salve os dados em um arquivo com nome; pesquisa_habitantes.txt.
+    - O progama deve ler o arquivo para exibir os dados salvos.
+
+ """
